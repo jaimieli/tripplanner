70aaeca1ff2dc5c35eca209b301c14da4a361012
Jaimie Li <jaimieli@gmail.com>
2014-08-20 19:25:39 -0400

    added db calls to main route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models = require('models');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  models.Hotel.find(function(err, hResults) {
+    models.Restaurant.find(function(err, rResults) {
+      models.ThingsToDo.find(function(err, tResults) {
+        res.render('index', { hotels: hResults, restaurants: rResults, thingsToDo: tResults, title: "Trip Planner" });
+      });
+    });
+  });
 });
 
 module.exports = router;
