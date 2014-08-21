37f38512b30169abb2ae72602005cea9035c833c
Gabriel Lebec <glebec@gmail.com>
2014-08-21 11:13:20 -0400

    loading bootstrap css and sass files, added navbar

--- app.js
+++ app.js
@@ -26,19 +26,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-app.use('/', routes);
-app.use('/users', users);
 
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  swig.setDefaults({cache: false});
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+console.log({
+    src: __dirname + '/assets', //where the sass files are
+    dest: __dirname + '/public/stylesheets', //where css should go
+    // includePaths: __dirname + '/assets/stylesheets',
+    debug: true // obvious
+  })
+
 
 app.use(
   sass.middleware({
@@ -49,28 +45,44 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  swig.setDefaults({cache: false});
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+
 /// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
